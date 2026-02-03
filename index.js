433b2bc713ba6e6d58da6bd06442f1252ebbc997
Pranav-Karra-3301 <pranavkarra001@gmail.com>
2026-02-03 14:22:08 -0500

    Add variables and data types

---
+++ index.js
@@ -0,0 +1,21 @@
+// Part 1: JavaScript Basics
+
+// === Variables and Data Types ===
+
+// Integer variable
+let score = 100;
+
+// Float variable
+let temperature = 36.6;
+
+// String variable
+let greeting = "Hello, JavaScript!";
+
+// Boolean variable
+let isLearningFun = true;
+
+// Print all variables
+console.log("score:", score);
+console.log("temperature:", temperature);
+console.log("greeting:", greeting);
+console.log("isLearningFun:", isLearningFun);
